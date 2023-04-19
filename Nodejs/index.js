84d3d2e8f87762fcbd9e90105d378bc2d84aa9c8
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-04-19 11:20:31 +0530

    added two npm packages

---
+++ Nodejs/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var mySuperHeroName = superheroes.random();
+
+var supervillains = require("supervillains");
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperHeroName);
+console.log(mySuperVillainName);
