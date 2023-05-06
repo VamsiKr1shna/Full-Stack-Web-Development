bda1b619241832d112b0a75e17e7864a83bf40d8
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-05-06 13:29:13 +0530

    shortened const and html in get

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello");   
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
 });
 
 app.listen(3000, function () {
