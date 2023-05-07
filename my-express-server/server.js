b03f81f1e9818232a5119ff970e6044bad286703
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-05-07 13:38:35 +0530

    added contact,about and hobbies route

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
+app.get("/about", (req, res) => {
+  res.send("hello My name is Vamsi");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact me at : pvamsikri2003@gmail.com");
+});
+
+app.get("/hobbies", (req, res) => {
+  res.send("Coffee and Tea");
+});
+
 app.listen(3000, function () {
   console.log("Server started at 3000");
 });
