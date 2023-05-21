1bb85ba9375806839900b7fe7d06cf5cf023b071
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-05-21 19:34:17 +0530

    added post and get methods

--- NewsLetter-Signup/app.js
+++ NewsLetter-Signup/app.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 const app = express();
 
-app.get("/",function (req,res) {
-    res.send("Server is on");
-})
+app.use(express.static("public")); //server can access the static files like css
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000,function () {
-    console.log("Server is running on 3000");
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var emailID = req.body.emailID;
+  console.log(firstName + " " + lastName + " " + emailID);
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on 3000");
+});
+
+
+
+
+
+
+
+
+
+//f8e9d8b740ea45584fc83872332ce773-us21 (mailchimp api key)
