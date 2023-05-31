49cab36acd04365c80773b3f98a39f7118443c2f
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-05-31 10:17:10 +0530

    added post method to send userdata to mailchimp

--- NewsLetter-Signup/app.js
+++ NewsLetter-Signup/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
+const https = require("https");
 
 app.use(express.static("public")); //server can access the static files like css
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,10 +12,38 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var emailID = req.body.emailID;
-  console.log(firstName + " " + lastName + " " + emailID);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const emailID = req.body.emailID;
+
+  const data = {
+    members: [
+      {
+        email_address: emailID,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us21.api.mailchimp.com/3.0/lists/400056784e";
+
+  const options = {
+    method: "POST",
+    auth: "vulcan1601:f8e9d8b740ea45584fc83872332ce773-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function () {
