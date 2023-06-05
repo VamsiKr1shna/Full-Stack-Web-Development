f5419e2cbc0e30ce8299106d8af19a1973d8ad14
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-06-05 20:36:44 +0530

    added ejs markers

--- TodoList_V1/app.js
+++ TodoList_V1/app.js
@@ -3,10 +3,18 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello from simple server :)");
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+  if (currentDay == 6 || currentDay == 0) day = "Weekend";
+  else day = "Weekday";
+
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
-  console.log("> Server is up and running on port : ");
+  console.log("> Server is up and running on port 3000 : ");
 });
