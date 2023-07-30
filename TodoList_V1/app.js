3a869e438d51b9a2ca9616f84b22f4f538ea1168
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-07-30 08:59:25 +0530

    installed new modules

--- TodoList_V1/app.js
+++ TodoList_V1/app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-  if (currentDay == 6 || currentDay == 0) day = "Weekend";
-  else day = "Weekday";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-IN", options);
 
   res.render("list", { kindOfDay: day });
+});
+
+app.post("/", function (req, res) {
+  var input = req.body.newItem;
+  console.log(input);
 });
 
 app.listen(3000, function () {
