36ce792da8e71e277401575cfce7741318df860a
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-08-28 20:51:11 +0530

    added socket

--- IITH WorkShop/server.js
+++ IITH WorkShop/server.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const socketIo = require("socket.io");
 const port = process.env.PORT || 5000;
-app.listen(5001, () => console.log("Listening on port ${5001}"));
-app.g
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+io.on("connection", (socket) => {
+  console.log("client connected: ", socket.id);
+
+  socket.join("clock-room");
+
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
+  });
+});
+
+setInterval(() => {
+  io.to("clock-room").emit("time", new Date());
+}, 1000);
+
+app.listen(port, () => console.log("Listening on port 5000"));
+
+app.get("/dummyApi", (req, res) => {
+  const reqData = req.query.data;
+  const reqName = req.query.name;
+  res.send(reqData + " " + reqName);
+});
+x;
