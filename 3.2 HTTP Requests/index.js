152d6b90aa7d7b5cd9e6455c1a9093e495b49978
VamsiKr1shna <pvamsikri2003@gmail.com>
2023-08-29 21:06:34 +0530

    tested different http requests

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -2,9 +2,33 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("<h1>Hello h1</h1>");
 });
 
+app.get("/aboutme", (req, res) => {
+  res.send("<p>About me</p> ");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h3>Contact me : 8555995320</h3>");
+});
+
+app.post("/register",(req,res)=>{
+  res.sendStatus(201);
+})
+
+app.put("/user/vamsi",(req,res)=>{
+  res.sendStatus(200);
+})
+
+app.patch("/user/vamsi",(req,res)=>{
+  res.sendStatus(200);
+})
+
+app.delete("/user/vamsi",(req,res)=>{
+  res.sendStatus(200);
+})
+
 app.listen(port, () => {
-  console.log("server is started!!");
+  console.log("server is started!", port);
 });
