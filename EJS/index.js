45ddedd69789d450e90f5b2b0cbf6966c150a6fb
Vamsi Krishna <104820101+VamsiKr1shna@users.noreply.github.com>
2023-11-04 19:50:18 +0530

    added parameters

--- EJS/index.js
+++ EJS/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const today = new Date();
+var currDay = today.getDay();
+const dayArr = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var adviceMessage = "Work Hard!!";
 
+if(currDay == 0 || currDay == 6 ){
+  adviceMessage = "So Rest Well";
+}
+
+var dayObj = {
+  day : dayArr[currDay],
+  advice : adviceMessage
+};
 app.get('/' , (req , res)=>{
 
-  res.render("index.ejs", {day : "Monday",advice : "Work Hard"});
+  res.render("index.ejs", dayObj);
 
 })
 
-app.listen(port, () => `Server running on port ${port} 🔥`);
+app.listen(port, console.log( `Server running on port ${port} `));
